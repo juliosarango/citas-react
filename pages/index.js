6dab5aa34a83df47c6ce179ff4659d88bfbe64f6
Julio Sarango <jsarangoq@gmail.com>
2018-12-18 23:45:04 -0500

    first commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from "../components/Head";
+
+class Index extends Component {
+    render() {
+        return (
+            <div>
+                <Header title="Administrador de citas" />
+            </div>
+        );
+    }
+}
+
+export default Index;
